b2334b29dbbb7f90e4280c6069e3495366a5b5c4
t-houssian <tylerhoussian@gmail.com>
2026-03-07 22:34:25 -0600

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import { ReactNode } from 'react';
 import { lora } from './fonts';
+import Script from 'next/script';
+
+const GOOGLE_ANALYTICS_ID = 'G-SWFSW2Z76Q';
 
 export const metadata = {
   title: 'Megan Houssian Art',
@@ -13,6 +16,20 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_ANALYTICS_ID}');
+          `}
+        </Script>
+      </head>
       <body className={`${lora.className} bg-ivory text-brown min-h-screen flex flex-col`}>
         <NavBar />
         <main className="flex-grow">{children}</main>
